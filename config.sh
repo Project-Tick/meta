d43266dd506ef36c89cad46f35f3dc14232dff3c
YongDo-Hyun <froster12@naver.com>
2025-12-13 20:23:51 +0300

    Add auto-update workflow and configuration files

---
+++ config.sh
@@ -0,0 +1,18 @@
+export META_UPSTREAM_URL="https://github.com/Project-Tick/meta-upstream.git"
+export META_LAUNCHER_URL="https://github.com/Project-Tick/meta-launcher.git"
+
+
+export META_UPSTREAM_DIR="upstream"
+export META_LAUNCHER_DIR="launcher"
+export DEPLOY_TO_FOLDER=false
+export DEPLOY_FOLDER=/app/public/v1
+export DEPLOY_FOLDER_USER=http
+export DEPLOY_FOLDER_GROUP=http
+
+export UPSTREAM_SSH="/home/samet/meta/config/key_upstream"
+export LAUNCHER_SSH="/home/samet/meta/config/key_launcher"
+export DEPLOY_TO_GIT=true
+export GIT_AUTHOR_NAME="Project Tick Bot"
+export GIT_AUTHOR_EMAIL="github@bot.yongdohyun.org.tr"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
